17649224c8bcada57ef690d66408595cf77c97e5
Mohamed Yasser <51481220+shnki@users.noreply.github.com>
2023-09-01 12:46:57 +0300

    response structure

--- api/countries.js
+++ api/countries.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", async (req, res) => {
+const countryList = require("./data.js");
+router.get("/:country", async (req, res) => {
+  const countryData = countryList.filter(
+    (obj) => obj.code == req.params.country.toUpperCase()
+  );
   try {
     res.json({
       status: 200,
-      message: "Get countries",
-      flag: req.protocol + "://" + req.get("host") + req.originalUrl,
+      message: "Get country",
+      data: {
+        ...countryData[0],
+        flag:
+          req.protocol +
+          "://" +
+          req.get("host") +
+          "/flags/" +
+          req.params.country.toLowerCase() +
+          ".png",
+      },
     });
   } catch (error) {
     console.log(error);
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product.js");
 const countries = require("./api/countries.js");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/countries", countries);
+app.use("/api/countries/", countries);
 app.use(express.static("public"));
 
 const PORT = process.env.port || 8080;
