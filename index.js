b4d40bcb23b33fa7fe1db7aff09e82d3c6fc495f
Mohamed Yasser <51481220+shnki@users.noreply.github.com>
2023-09-16 19:18:47 +0300

    flagyGame endpoint

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const countries = require("./api/countries.js");
 const mainPage = require("./api/mainPage.js");
 const flag = require("./api/flag.js");
+const flagyGame = require("./api/flagyGame.js");
 
 app.use(express.static("public"));
 app.use(express.json({ extended: false }));
 
 app.use("/api/countries/", countries);
 app.use("/", mainPage);
+app.use("/FlagyGame", flagyGame);
 app.use("/flag", flag);
 const PORT = process.env.port || 8080;
 
